1ea0e760cbd53a1a1e10427655010610c7c2bb30
Aleix <algafix@gmail.com>
2020-08-31 17:38:39 +0200

    Bug in convert data

--- osnma_core.py
+++ osnma_core.py
@@ -1,9 +1,11 @@
-import bitstring as bs
+"""Module that implements the basic OSNMA actions
+"""
+
 import math
-import sys
 import hmac
-import ecdsa
 import hashlib
+import ecdsa
+import bitstring as bs
 import auxiliar_data.osnma_fields as osnma_fields
 import auxiliar_data.osnma_structures as osnma_structures
 import auxiliar_data.exceptions as osnma_exceptions
@@ -41,7 +43,7 @@ class OSNMACore:
         """
         try:
             if not isinstance(data, bs.BitArray):
-                    data = bs.BitArray(data)
+                data = bs.BitArray(data)
             return data
         except bs.CreationError:
             raise TypeError("Can't convert to BitArray: " + str(data))
@@ -225,14 +227,14 @@ class OSNMACore:
             self.pubk_path = pub_key
 
         try:
-            hash = self.__hash_table[hash_name]
+            hash_func = self.__hash_table[hash_name]
         except KeyError:
             raise TypeError("Hash not supported: " + hash_name)
 
         # Load key and create sign object
         with open(self.pubk_path) as f:
             try:
-                vk = ecdsa.VerifyingKey.from_pem(f.read(), hashfunc=hash)
+                vk = ecdsa.VerifyingKey.from_pem(f.read(), hashfunc=hash_func)
             except IOError as e:
                 print("I/O error({0}): {1}".format(e.errno, e.strerror))
 
@@ -468,7 +470,7 @@ class OSNMACore:
 
         if self.__merkle_root == None:
             raise AttributeError("Missing Merkle root")
-        
+
         # Create the pkr signature message
         message = bs.BitArray()
         for field in self.OSNMA_crypto['pkr_m']:
@@ -494,7 +496,8 @@ class OSNMACore:
         return node == self.__merkle_root
 
     def dms_pkr_process(self, dms_pkr):
-        """Fragment the dms_pkr message in its fields and autenticates the new pkr key calling to self.pkr_verification
+        """Fragment the dms_pkr message in its fields and autenticates the new pkr key calling to
+        self.pkr_verification
 
         :param dms_pkr Raw DMS-PRK message
         :type dms_pkr BitArray; formated String for bin, oct or hex
